ec7aa912f582fc1f14c88765f5ea6f768c9a37ee
EmirMervanKanun <mervankanun@gmail.com>
2024-03-26 15:17:28 +0100

    Button und so

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Image, TextInput} from 'react-native';
 
+
+var background;
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>"Verwalte deine Ein- und Ausgaben mit *Budget*. Verliere nie mehr den Überblick. Wie viel Willst du Sparen?" </Text>
+      <Text style={styles.text}>Hallo Tina : Was geht AAHAAHAAHAHAAAB!</Text> 
+      <Text style={styles.text}> Verliere nie mehr den Überblick. Wie viel Willst du Sparen?" </Text>
       <TextInput style={styles.text}> </TextInput>
-      <Button title="Jetz Sparen"></Button>
+      <Button onPress={onPressLearnMore} title=" Eimn Milliooiolnen Sparn "></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+function onPressLearnMore() {
+  // set the View background color to red
+  var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  App.setBackgroundColor(randomColor);
+
+  console.log('LOG ist ein LÜGER');
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -21,7 +31,7 @@ const styles = StyleSheet.create({
     padding: 30,
   },
   text: {
-    fontSize: 24,
+    fontSize: 30,
     textAlign: 'center',
     margin: 10,
   },
