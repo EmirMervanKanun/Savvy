61631d2144bdd7ec6d6bb2177c3dbb4d8e742e19
adrechsel169 <alena.drechsel@hof-university.de>
2024-04-23 17:43:06 +0200

    testi

--- Screens/Navigation/TransactionsList.js
+++ Screens/Navigation/TransactionsList.js
@@ -1,11 +1,13 @@
 import { ImageBackground, View, Text } from 'react-native';
 import React from 'react';
+import DropdownComponent from '../../Components/Dropdowns/Dropdown';
 
 export default function TransactionsList() {
   return (
     <ImageBackground source={require('../../assets/grid.png')} style={{flex: 1, resizeMode: "cover", justifyContent: "center"}}>
     <View style={{flex: 1, alignItems: "center", justifyContent:"center"}}>
       <Text>Transaktionen Liste</Text>
+      <DropdownComponent></DropdownComponent>
     </View>
     </ImageBackground>
   );
