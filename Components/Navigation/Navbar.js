4fb3f31d6880ae28db2d28ff4441583182b8156a
kpertek <kevin.pertek@gmx.de>
2024-04-23 14:25:31 +0200

    navbar als eigenes component

---
+++ Components/Navigation/Navbar.js
@@ -0,0 +1,161 @@
+import { StyleSheet, ImageBackground, View, Image, Text } from 'react-native';
+import { Home, Budget, FixedTransactions, TransactionsList, AddTransaction } from './index';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Svg, { Path } from 'react-native-svg';
+import { useFonts } from 'expo-font';
+import Header from '../TextComponents/Header.js';
+import OurFont from '../TextComponents/OurFont.js';
+import RadioDefaultText from '../TextComponents/RadioDefaultText.js';
+import NavText from '../TextComponents/NavText.js';
+import DateText from '../TextComponents/DateText.js';
+import ButtonText from '../TextComponents/ButtonText.js';
+import TitelAmountText from '../TextComponents/TitelAmountText.js';
+import PlaceholderText from '../TextComponents/PlaceholderText.js';
+import SavingsAmountText from '../TextComponents/SavingsAmountText.js';
+
+const Tab = createBottomTabNavigator();
+const screenOptions = {
+  tabBarShowLabel: false,
+  headerShown: false,
+  tabBarStyle: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    elevation: 0,
+    height: 75,
+    backgroundColor: "#d5dff4",
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    borderTopWidth: 0,
+  }
+};
+
+export default function Navbar() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Sora-Regular': require('../../assets/Fonts/Sora-Regular.ttf'),
+    'Sora-Bold': require('../../assets/Fonts/Sora-Bold.ttf'),
+    'Sora-SemiBold': require('../../assets/Fonts/Sora-SemiBold.ttf')
+  });
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+  return (
+    <ImageBackground source={require('../../assets/grid.png')} style={{flex: 1, resizeMode: "cover", justifyContent: "center"}}>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen 
+          name="Hauptmenü" 
+          component={Home}
+          options={{
+            tabBarIcon: ({focused}) => {
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}>
+                  <Image source={ focused ? require('../../assets/Icons/NavBar/HauptmenüFocused.png'): require('../../assets/Icons/NavBar/HauptmenüUnfocused.png')}
+                    style={{ width: 30, height: 30, }} 
+                  />
+                  <Text style={{fontSize: 12, color: focused ? "#424874": "#a1a6cc" }}>Hauptmenü</Text>
+                </View>
+              )
+            },
+          }} />
+        <Tab.Screen 
+          name="Sparziele" 
+          component={Budget}
+          options={{
+            tabBarIcon: ({focused}) => {
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}>
+                  <Image source={ focused ? require('../../assets/Icons/NavBar/SparzieleFocused.png'): require('../../assets/Icons/NavBar/SparzieleUnfocused.png')}
+                    style={{ width: 30, height: 30, }} 
+                  />
+                  <Text style={{fontSize: 12, color: focused ? "#424874": "#a1a6cc" }}>Sparziele</Text>
+                </View>
+              )
+            },
+          }} />
+        <Tab.Screen
+          name="Plus" 
+          component={AddTransaction}
+          options={{
+            tabBarIcon: ({focused}) => {
+              return (
+                <View style={{ flex: 1, alignItems: "center"}}>
+                  <View style={{ flexDirection: 'row', position: 'absolute', top: 0}}>
+                    <View style={{ flex: 1, backgroundColor: '#d5dff4'}}></View>
+                    <Svg
+                      width={75}
+                      height={61}
+                      viewBox='0 0 75 61'
+                      style={{backgroundColor: '#fff',}}
+                    >
+                      <Path
+                        d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+                        fill={'#d5dff4'}
+                      />
+                    </Svg>
+                    <View style={{ flex: 1, backgroundColor: '#d5dff4'}}></View>
+                  </View>
+                  <View 
+                      style={{
+                        top: -22.5,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        width: 50,
+                        height: 50,
+                        borderRadius: 25,
+                        backgroundColor: focused ? "#424874": "#a1a6cc",
+                      }}
+                  >
+                    <Image source={ focused ? require('../../assets/Icons/NavBar/PlusFocused.png'): require('../../assets/Icons/NavBar/PlusUnfocused.png')}
+                      style={{ width: 20.8, height: 20.8, }}/>
+                  </View>
+                </View>
+              )
+            },
+          }} />
+        <Tab.Screen 
+          name="Dauerauftrag" 
+          component={FixedTransactions}
+          options={{
+            tabBarIcon: ({focused}) => {
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}>
+                  <Image source={ focused ? require('../../assets/Icons/NavBar/DauerauftragFocused.png'): require('../../assets/Icons/NavBar/DauerauftragUnfocused.png')}
+                    style={{ width: 33, height: 30, }} 
+                  />
+                  <Text style={{fontSize: 12, color: focused ? "#424874": "#a1a6cc" }}>Dauerauftrag</Text>
+                </View>
+              )
+            },
+          }} />
+        <Tab.Screen 
+          name="Transaktionen" 
+          component={TransactionsList}
+          options={{
+            tabBarIcon: ({focused}) => {
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}>
+                  <Image source={ focused ? require('../../assets/Icons/NavBar/AusgabenFocused.png'): require('../../assets/Icons/NavBar/AusgabenUnfocused.png')}
+                    style={{ width: 30, height: 30, }} 
+                  />
+                  <Text style={{fontSize: 11, color: focused ? "#424874": "#a1a6cc" }}>Transaktionen</Text>
+                </View>
+              )
+            },
+          }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+    justifyContent: 'center',
+  },
+});
