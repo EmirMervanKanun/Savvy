090c9f0ad498f8be5342381e3491f2580bc9a577
EmirMervanKanun <mervankanun@gmail.com>
2024-04-23 14:12:15 +0200

    Buttons erstellt!

--- Screens/AddTransaction.js
+++ Screens/AddTransaction.js
@@ -1,11 +1,13 @@
 import {View, Text } from 'react-native';
 import React from 'react';
+import {ButtonBig, ButtonMid, ButtonSmall} from '../Components/Buttons/Buttons.js';
 
-export default function Saving() {
+export default function AddTransaction() {
   return (
     
     <View style={{flex: 1, alignItems: "center", justifyContent:"center"}}>
       <Text>Transaktionen Hinzufügen</Text>
+      
     </View>
   );
 }
--- Screens/Navigation/Home.js
+++ Screens/Navigation/Home.js
@@ -1,12 +1,24 @@
-import { ImageBackground, View, Text } from 'react-native';
+import { StyleSheet, ImageBackground, View, Text } from 'react-native';
 import React from 'react';
+import ButtonBig from '../../Components/Buttons/Buttons.js';
+import {ButtonMid} from '../../Components/Buttons/Buttons.js';
+import {ButtonSmall} from '../../Components/Buttons/Buttons.js';
 
 export default function Home() {
   return (
   <ImageBackground source={require('../../assets/grid.png')} style={{flex: 1, resizeMode: "cover", justifyContent: "center"}}>
     <View style={{flex: 1, alignItems: "center", justifyContent:"center"}}>
       <Text>Hauptmenü</Text>
+      <ButtonBig text="Los geht's!" />
+      <ButtonMid text="Los geht's!" img={require('../../Icons/NavBar/PlusUnfocused.png')}/>
+      <ButtonSmall text="Los geht's!" img={require('../../Icons/NavBar/PlusUnfocused.png')} isRed='1'/>
     </View>
     </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  buttoncolor: {
+    backgroundColor: COLORS.redMid,
+  },
+});
