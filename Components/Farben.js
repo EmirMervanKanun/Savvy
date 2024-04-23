82c19e567bfae60fad6a68a3d3b499ac26d739f3
EmirMervanKanun <mervankanun@gmail.com>
2024-04-23 15:20:40 +0200

    Farben machen das Leben schön - Tina

---
+++ Components/Farben.js
@@ -0,0 +1,14 @@
+export default COLORS = {
+    schriftDark: '#2D3142',
+    schriftMid: '#7D8091',
+    schriftLight: '#DCDEE4',
+    primaryDark: '#424874',
+    primaryMid: '#A1A6CC',
+    primaryLight: '#D5DFF4',
+    greenDark: '#2ECC71',
+    greenMid: '#95E3B6',
+    greenLight: '#D2F1E0',
+    redDark: '#DC3545',
+    redMid: '#EA979F',
+    redLight: '#F5D7DE',
+}
