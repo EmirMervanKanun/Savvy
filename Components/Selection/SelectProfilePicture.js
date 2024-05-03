0d2e0173d665dac05496e3efe590e262890824e9
adrechsel169 <alena.drechsel@hof-university.de>
2024-05-03 15:18:05 +0200

    brudi wie funktionieren diese icons in den buttons °°

--- Components/Selection/SelectProfilePicture.js
+++ Components/Selection/SelectProfilePicture.js
@@ -3,31 +3,49 @@ import { StyleSheet, Image, TouchableOpacity, View, FlatList, Text } from "react
 import ButtonText from "../TextComponents/ButtonText";
 import PlaceholderText from "../TextComponents/PlaceholderText";
 import COLORS from "../Farben"
-
-const profilePicturesData = [
-    { label: 'default', img:<Image source={require('../../Icons/ProfilePictures/profilePictureDefault.png')}/>, value: '0' },         //leerer Eintrag, damit 1. Eintrag nicht hinter Auswahl rutscht
-    { label: 'pb1', img:<Image source={require('../../Icons/ProfilePictures/profilePicture1.png')}/>, value: '1' },
-    { label: 'pb2', img:<Image source={require('../../Icons/ProfilePictures/profilePicture2.png')}/>, value: '2' },
-    { label: 'pb3', img:<Image source={require('../../Icons/ProfilePictures/profilePicture3.png')}/>, value: '3' },
-    { label: 'pb4', img:<Image source={require('../../Icons/ProfilePictures/profilePicture4.png')}/>, value: '4' },
-    { label: 'pb5', img:<Image source={require('../../Icons/ProfilePictures/profilePicture5.png')}/>, value: '5' },
-    { label: 'pb6', img:<Image source={require('../../Icons/ProfilePictures/profilePicture6.png')}/>, value: '6' },
-    { label: 'pb7', img:<Image source={require('../../Icons/ProfilePictures/profilePicture7.png')}/>, value: '7' },
-    { label: 'pb8', img:<Image source={require('../../Icons/ProfilePictures/profilePicture8.png')}/>, value: '8' },
-    { label: 'pb9', img:<Image source={require('../../Icons/ProfilePictures/profilePicture9.png')}/>, value: '9' },
-    { label: 'pb10', img:<Image source={require('../../Icons/ProfilePictures/profilePicture10.png')}/>, value: '10' },
-    { label: 'pb11', img:<Image source={require('../../Icons/ProfilePictures/profilePicture11.png')}/>, value: '11' },
-];
+import Header from "../TextComponents/Header"; 
+import { ButtonMid } from "../Buttons/Buttons";
 
 const ProfilePictureSelector = () => {
-    const placeholder = profilePicturesData[0].img;      //Default-Profilbild ist standardmäßig ausgewählt
+    /*const placeholder = profilePicturesData[0].img;      //Default-Profilbild ist standardmäßig ausgewählt
+    const [selectedImg, setSelectedIcon] = useState(placeholder);
+    const [data, setData] = useState(profilePicturesData);
+*/
+    const [isClicked, setIsClicked] = useState(false);
+
+
     return (
         <View>
-            <Text>
-                Wähle ein Profilbild aus:
-            </Text>
-            
-                <Image source={require('../../Icons/ProfilePictures/profilePictureDefault.png')}/>
+            <TouchableOpacity
+                style={styles.selectedProfilePicture}
+                onPress={() => {setIsClicked(!isClicked);}}>
+                <Image source={require('../../Icons/ProfilePictures/profilePictureDefault.png')} style={styles.selImg}></Image>
+            </TouchableOpacity>
+
+            {isClicked ? (        //wenn geklickt, dann Dropdown Area anzeigen
+                <View style={styles.selectionArea}>
+                    <Header>Profilbild auswählen</Header>
+                    <View style={styles.images}>      
+                        <Image source={require('../../Icons/ProfilePictures/profilePictureDefault.png')} style={styles.image}></Image>
+                        <Image source={require('../../Icons/ProfilePictures/profilePicture1.png')} style={styles.image}></Image>
+                        <Image source={require('../../Icons/ProfilePictures/profilePicture2.png')} style={styles.image}></Image>
+                        <Image source={require('../../Icons/ProfilePictures/profilePicture3.png')} style={styles.image}></Image>
+                        <Image source={require('../../Icons/ProfilePictures/profilePicture4.png')} style={styles.image}></Image>
+                        <Image source={require('../../Icons/ProfilePictures/profilePicture5.png')} style={styles.image}></Image>
+                        <Image source={require('../../Icons/ProfilePictures/profilePicture6.png')} style={styles.image}></Image>
+                        <Image source={require('../../Icons/ProfilePictures/profilePicture7.png')} style={styles.image}></Image>
+                        <Image source={require('../../Icons/ProfilePictures/profilePicture8.png')} style={styles.image}></Image>
+                        <Image source={require('../../Icons/ProfilePictures/profilePicture9.png')} style={styles.image}></Image>
+                        <Image source={require('../../Icons/ProfilePictures/profilePicture10.png')} style={styles.image}></Image>
+                        <Image source={require('../../Icons/ProfilePictures/profilePicture11.png')} style={styles.image}></Image>
+                    </View>
+                    <ButtonMid text="Abbrechen" img={<Image source={require('../../Icons/Button/cancel.png')} />}></ButtonMid>
+                    <ButtonMid text="Speichern" img={'../../Icons/Button/save.png'}></ButtonMid>
+                </View>
+            ):(
+                null
+            )}
+
             
         </View>
 
@@ -36,7 +54,55 @@ const ProfilePictureSelector = () => {
 
 export default ProfilePictureSelector;
 
+const styles = StyleSheet.create({
+    selImg: {
+        width: 120,
+        height: 120,
+    },
+    selectionArea: {
+        width: 292,
+        paddingVertical: 16,
+        backgroundColor: COLORS.primaryLight,
+        borderRadius: 15,
+        alignItems: 'center',
+        gap: 24,
+    },
+    images: {
+        width: 250,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        gap: 16,
+    },
+    image: {
+        width: 70,
+        height: 70,
+    },
+});
+
+
 /*
+
+{isClicked ? (        //wenn geklickt, dann Dropdown Area anzeigen
+                <View style={styles.selectionArea}>
+                    <FlatList data={data} renderItem={({item})=>{
+                        return(
+                            <TouchableOpacity style={styles.dropdownItem} onPress={()=>{
+                                setSelectedIcon(item.img);
+                                setIsClicked(false);
+                            }}>
+                                <View style={styles.itemContent}> 
+                                    <View>{item.img}</View>
+                                </View>
+                            </TouchableOpacity>
+                        )
+                    }}/>
+                </View>
+            ):(
+                null
+            )}
+
+
 
 const CategoriesDropdown = () => {
     const placeholder = dropdownData[0].label;      //"Wähle eine Kategorie aus..." ist standardmäßig ausgewählt
