d99a653195ca3953ba9a07f5400c85331918d57e
EmirMervanKanun <mervankanun@gmail.com>
2024-05-16 11:55:35 +0200

    Home for testing changed

--- Screens/Navigation/Home.js
+++ Screens/Navigation/Home.js
@@ -1,10 +1,7 @@
 import { StyleSheet, ImageBackground, View, Text } from 'react-native';
 import React from 'react';
-import HomeIncome from '../../Components/OBJHome/HomeEA';
-import HomeHeader from '../../Components/OBJHome/HomeHeader';
-import HomeEA from '../../Components/OBJHome/HomeEA';
-import Kategorie from '../../Components/SonstSonstiges/Categorielist';
-import Categorie from '../../Components/SonstSonstiges/Categorielist';
+import ButtonBig from '../../Components/Buttons/Buttons';
+import {ButtonMid, ButtonSmall, ButtonIcon, ButtonBack, ButtonSettings} from '../../Components/Buttons/Buttons';
 
 
 export default function Home() {
@@ -12,22 +9,21 @@ export default function Home() {
   <ImageBackground source={require('../../assets/grid.png')} style={{flex: 1, resizeMode: "cover", justifyContent: "center"}}>
     <View style={{flex: 1, alignItems: "center", justifyContent:"center"}}>
       <Text>Hauptmenü</Text>
-      <HomeEA></HomeEA>
       <ButtonBig text="Los geht's!" onPress={()=>console.log("test")}/>
       <ButtonMid text="Bearbeiten" img={require('../../Icons/Button/edit.png')}/>
       <ButtonSmall text="Löschen" img={require('../../Icons/Button/delete.png')} isRed='1'/>
       <ButtonIcon img={require('../../Icons/Button/delete.png')} isRed='1'/>
       <ButtonIcon img={require('../../Icons/Button/save.png')}/>
-      <Categorie/>
+      <ButtonBack/>
+      <ButtonSettings/>
+
     </View>
     </ImageBackground>
   );
 }
 
-/*const styles = StyleSheet.create({
-  buttoncolor: {
-    backgroundColor: COLORS.redMid,
-  },
-});*/
+const styles = StyleSheet.create({
+  
+});
 
 //<HomeHeader img={require('../../Icons/ProfilePictures/profilePicture1.png')}/>
