5fb7503866bb89adbd9af8d2afce6e588ba2a277
adrechsel169 <alena.drechsel@hof-university.de>
2024-05-23 19:15:02 +0200

    hier wurde auch mal gewischt und componenten gefixt (:

--- Screens/Navigation/Home.js
+++ Screens/Navigation/Home.js
@@ -1,18 +1,37 @@
-import { StyleSheet, ImageBackground, View, Text } from 'react-native';
+import { StyleSheet, ImageBackground, View, Text, Image } from 'react-native';
 import React from 'react';
-import HomeIncome from '../../Components/OBJHome/HomeEA';
-import HomeHeader from '../../Components/OBJHome/HomeHeader';
-import HomeEA from '../../Components/OBJHome/HomeEA';
-import Kategorie from '../../Components/SonstSonstiges/Categorielist';
-import Categorie from '../../Components/SonstSonstiges/Categorielist';
+import Categories from '../../Components/SonstSonstiges/Categorielist';
+import Notifcation from '../../Components/SonstSonstiges/Notifications';
+
+const listCategories = [
+  { label: 'Lebensmittel', icon: require('../../Icons/Categories/groceries.png') },
+  { label: 'Miete', icon: require('../../Icons/Categories/rent.png') },
+  { label: 'Klamotten', icon: require('../../Icons/Categories/clothes.png') },
+  { label: 'Technik', icon: require('../../Icons/Categories/devices.png') },
+  { label: 'Transportmittel', icon: require('../../Icons/Categories/transportation.png') },
+  { label: 'Medikamente', icon: require('../../Icons/Categories/medication.png') },
+  { label: 'Haustier', icon: require('../../Icons/Categories/pets.png') },
+];
+
+const notificationList1 = [
+  { label: 'Mitteilungen erhalten'},
+];
+
+const notificationList2 = [
+  { label: 'Mitteilung 1'},
+  { label: 'Mitteilung 2'},
+  { label: 'Mitteilung 3'},
+];
 
 
 export default function Home() {
   return (
-  <ImageBackground source={require('../../assets/grid.png')} style={{flex: 1, resizeMode: "cover", justifyContent: "center"}}>
-    <View style={{flex: 1, alignItems: "center", justifyContent:"center"}}>
-      <Text>Hauptmenü</Text>
-    </View>
+    <ImageBackground source={require('../../assets/grid.png')} style={{ flex: 1, resizeMode: 'cover', justifyContent: 'center' }}>
+      <View style={{ flex: 1 }}>
+        <Notifcation props={notificationList1} />
+        <Notifcation props={notificationList2} />
+        <Categories props={listCategories} />
+      </View>
     </ImageBackground>
   );
 }
