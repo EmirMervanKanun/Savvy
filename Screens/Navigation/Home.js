0a5c767711191fba66091e4659ccb74773eac9c0
adrechsel169 <alena.drechsel@hof-university.de>
2024-05-24 12:13:06 +0200

    grid bilder gelöscht (endlich)

--- Screens/Navigation/Home.js
+++ Screens/Navigation/Home.js
@@ -1,26 +1,11 @@
-import { StyleSheet, ImageBackground, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import React from 'react';
-import HomeIncome from '../../Components/OBJHome/HomeEA';
-import HomeHeader from '../../Components/OBJHome/HomeHeader';
-import HomeEA from '../../Components/OBJHome/HomeEA';
-import Kategorie from '../../Components/SonstSonstiges/Categorielist';
-import Categorie from '../../Components/SonstSonstiges/Categorielist';
 
 
 export default function Home() {
   return (
-  <ImageBackground source={require('../../assets/grid.png')} style={{flex: 1, resizeMode: "cover", justifyContent: "center"}}>
-    <View style={{flex: 1, alignItems: "center", justifyContent:"center"}}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Hauptmenü</Text>
     </View>
-    </ImageBackground>
   );
 }
-
-/*const styles = StyleSheet.create({
-  buttoncolor: {
-    backgroundColor: COLORS.redMid,
-  },
-});*/
-
-//<HomeHeader img={require('../../Icons/ProfilePictures/profilePicture1.png')}/>
--- Screens/Navigation/TransactionsList.js
+++ Screens/Navigation/TransactionsList.js
@@ -1,12 +1,11 @@
-import { ImageBackground, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import React from 'react';
+
 
 export default function TransactionsList() {
   return (
-    <ImageBackground source={require('../../assets/grid.png')} style={{flex: 1, resizeMode: "cover", justifyContent: "center"}}>
-    <View style={{flex: 1, alignItems: "center", justifyContent:"center"}}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Transaktionen Liste</Text>
     </View>
-    </ImageBackground>
   );
 }
