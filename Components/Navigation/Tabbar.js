798d7e343f56c3a32ea428e1d2dabda8ece4c702
adrechsel169 <alena.drechsel@hof-university.de>
2024-05-28 12:52:07 +0200

    tabbars gefixt + wrapper componenten zum einbinden (war ein kampf)

--- Components/Navigation/Tabbar.js
+++ Components/Navigation/Tabbar.js
@@ -1,41 +1,75 @@
 import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { Text, View } from 'react-native';
-import Budget from '../../Screens/Navigation/Budget';
-import Saving from '../../Screens/Navigation/Saving';
-import COLORS from '../Farben'
+import { Text, View, StyleSheet } from 'react-native';
+import COLORS from '../Farben';
+import HeaderText from '../TextComponents/Header';
 
 const Tab = createMaterialTopTabNavigator();
-const screenOptions ={
-    swipeEnabled: false,
-    tabBarShowLabel: true,
-    tabBarIndicatorStyle: { 
-        backgroundColor: COLORS.primaryLight,
-    },
-    tabBarStyle: {
-        height: 96,
-        backgroundColor: COLORS.primaryLight,
-        justifyContent: 'center',
-    },
-}
 
-function MyTabs() {
+const screenOptions = {
+  swipeEnabled: false,
+  tabBarShowLabel: true,
+  tabBarIndicatorStyle: {
+    display: 'none',                // remove indicator
+  },
+  tabBarStyle: {
+    height: 80,
+    backgroundColor: COLORS.primaryLight,
+    justifyContent: 'flex-end',
+    elevation: 0,                   // remove shadow on Android
+    shadowColor: 'transparent',     // remove shadow on iOS
+  },
+};
+
+function MyTabs({ tab1, tab2 }) {
   return (
     <Tab.Navigator screenOptions={screenOptions}>
-      <Tab.Screen 
-        name="Home" 
-        component={Budget} 
+      <Tab.Screen
+        name={tab1.title}
+        component={tab1.component}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <View style={[
+              styles.tabbarLabel, 
+              { backgroundColor: focused ? "white" : COLORS.primaryLight }
+            ]}>
+              <Text><HeaderText>{tab1.title}</HeaderText></Text>
+            </View>
+          ),
+        }}
       />
-      <Tab.Screen 
-        name="Settings" 
-        component={Saving}
+      <Tab.Screen
+        name={tab2.title}
+        component={tab2.component}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <View style={[
+              styles.tabbarLabel, 
+              { backgroundColor: focused ? "white" : COLORS.primaryLight }
+            ]}>
+              <Text><HeaderText>{tab2.title}</HeaderText></Text>
+            </View>
+          ),
+        }}
       />
     </Tab.Navigator>
   );
 }
 
-export default function Tabbar() {
+export default function Tabbar({ tab1, tab2 }) {
   return (
-    <MyTabs />
+    <MyTabs tab1={tab1} tab2={tab2} />
   );
 }
+
+const styles = StyleSheet.create({
+  tabbarLabel: {
+    width: 156,
+    height: 56,
+    alignItems: "center",
+    justifyContent: "center",
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    marginBottom: -10, 
+  }
+});
