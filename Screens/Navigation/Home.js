d37e626ebaf09fea0fa3eef972ce6587ae3b645d
mlphung <mai.phung@hof-university.de>
2024-06-06 11:58:02 +0200

    home änderungen

---
+++ Screens/Navigation/Home.js
@@ -0,0 +1,10 @@
+import { ImageBackground, View, Text } from 'react-native';
+import React from 'react';
+
+export default function Home() {
+  return (
+    <View style={{flex: 1, alignItems: "center", justifyContent:"center"}}>
+      <Text>Hauptmenü</Text>
+    </View>
+  );
+}
