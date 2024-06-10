15f666340f44883e164b89dd0224d17f93e9493b
mlphung <mai.phung@hof-university.de>
2024-06-10 20:00:55 +0200

    new onboarding (zwischenstand)...aber mit problem

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import  Navbar  from './Components/Navigation/Navbar.js';
+
+import Navbar from './Components/Navigation/Navbar.js';
+
 import { useFonts } from 'expo-font';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
-import MyStack from '../Budget/Screens/Home/Home.js';
-import Home from '../Budget/Screens/Home/Home.js';
+
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Stack = createNativeStackNavigator();
+import Onboarding from './Screens/Onboarding/Onboarding';
+
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 export default function App() {
+
   const [fontsLoaded, fontError] = useFonts({
     'Sora-Regular': require('./assets/Fonts/Sora-Regular.ttf'),
     'Sora-Bold': require('./assets/Fonts/Sora-Bold.ttf'),
@@ -16,14 +26,39 @@ export default function App() {
   if (!fontsLoaded && !fontError) {
     return null;
   }
-  return(
-    <SafeAreaProvider>
-      <NavigationContainer>
-          <Navbar />
-      </NavigationContainer>
-    </SafeAreaProvider>
-  );
-}
 
-    
-  
+  const [isAppFirstLaunched, setIsAppFirstLaunched] = React.useState(null);
+
+  React.useEffect(async () => {
+    const appData = await AsyncStorage.getItem('isAppFirstLaunched');
+    if (appData == null) {
+      setIsAppFirstLaunched(true);
+      AsyncStorage.setItem('isAppFirstLaunched', 'false');
+    } else {
+      setIsAppFirstLaunched(false);
+    }
+  }, []);
+
+  return (
+    isAppFirstLaunched !== null && (
+      <SafeAreaProvider>
+        <NavigationContainer>
+          {/*}
+          <Stack.Navigator>
+            {isAppFirstLaunched && (
+              <Stack.Screen
+                name="Onboarding"
+                component={Onboarding}
+              />
+            )}
+          </Stack.Navigator>
+          */}
+          <Navbar />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    )
+  );
+};
+
+
+
--- Screens/Onboarding/Onboarding.js
+++ Screens/Onboarding/Onboarding.js
@@ -1,56 +1,189 @@
-import React, { useState, useRef } from "react";
-import { View, StyleSheet, FlatList, Animated } from "react-native";
-import slides from "../../Components/Onboarding/slides";
-import OnboardingItem from "../../Components/Onboarding/OnboardingItem";
-import CircleProgress from "../../Components/Onboarding/CircleProgress";
+import React from 'react';
+import { SafeAreaView, StyleSheet, Dimensions, FlatList, View } from 'react-native';
+import COLORS from '../../Components/Farben';
+import Header from '../../Components/TextComponents/Header';
+import PlaceholderText from '../../Components/TextComponents/PlaceholderText';
+import Button from '../../Components/Buttons/Button';
 
-export default Onboarding = () => {  
+import Registry from '../Onboarding/Registry';
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const scrollX = useRef(new Animated.Value(0)).current;
-  const slidesRef = useRef(null);
+const {width, height} = Dimensions.get('window');
 
-  const viewableItemsChanged = useRef(({ viewableItems }) => {
-    setCurrentIndex(viewableItems[0].index);
-  }).current;
+const slides = [
 
-  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+  {
+    id: '1',
+    image: require('../../Icons/Onboarding/onboarding1.png'),
+    title: 'Ausgaben tracken',
+    description: 'Nie wieder den Überblick verlieren - mit unserer App behältst du deine Ausgaben im Griff!',
+  },
 
+  {
+    id: '2',
+    image: require('../../Icons/Onboarding/onboarding2.png'),
+    title: 'Daueraufträge festlegen',
+    description: 'Trage deine Daueraufträge ein und behalte stets im Blick, was du zahlst und wann du Geld erhältst - nie wieder vergisst du, was wichtig ist!',
+  },
+
+  {
+    id: '3',
+    image: require('../../Icons/Onboarding/onboarding3.png'),
+    title: 'Sparziele bestimmen',
+    description: 'Erreiche deine finanziellen Träume, indem du klare Sparziele setzt und sie mit unserer App spielend leicht erreichst!',
+  },
+
+  {
+    id: '4',
+    image: require('../../Icons/Onboarding/logoOnboarding.png'),
+    title: 'Ausgaben tracken',
+    description: 'Dein Budget im Griff mit Savvy – Finanzielle Freiheit leicht gemacht!',
+  },
+
+];
+
+const Slides = ({ item }) => {
+  return (
+    <View style={{ alignItems: 'center' }}>
+      <Image source={item.image} style={styles.image} />
+      <Text style={styles.title}><Header>{item.title}</Header></Text>
+      <Text style={styles.description}><PlaceholderText>{item.description}</PlaceholderText></Text>
+    </View>
+  );
+};
+
+const Onboarding = ({ navigation }) => {
+
+  const [currentIndex, setCurrentSlideIndex] = React.useState(0);
+
+  const updateCurrentSlideIndex = e => {
+    const contentOffsetX = e.nativeEvent.contentOffset.x;
+    const currentIndex = Math.round(contentOffsetX / width);
+    setCurrentSlideIndex(currentIndex);
+  };
+
+  const Footer = () => {
     return (
-      <View style = {styles.container}>
+      <View style={StyleSheet.footerContainer}>
 
-        <CircleProgress data = {slides} scrollX={scrollX}/>
+        {/* Button auf dem letzten Slide */}
+        <View style={{ marginBottom: 20 }}>
 
-        <View style = {{flex: 3}}>
-          <FlatList 
-            data = {slides} 
-            renderItem = { ({item}) => <OnboardingItem item = {item} />}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            pagingEnabled
-            bounces = {false}
-            keyExtractor={(item) => item.id}
-            onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: scrollX } } }], { 
-              useNativeDriver: false, 
-            })}
+          {currentSlideIndex == slides.length - 1 ? (
+            <View>
+              <Button
+                text='Los geht`s'
+                onPress={() => navigation.navigate("Registry")}
+              />
+            </View>
+          ) : (null)
+          }
 
-            scrollEventThrottle={32}
+        </View>
 
-            onViewableItemsChanged={viewableItemsChanged}
+        {/* Circle Indicator*/}
+        <View
+          style={styles.indicatorContainer}>
 
-            viewabilityConfig={viewConfig}
-            
-          />
+          {slides.map((_, index) => (
+            <View
+              key={index}
+              style={[
+                styles.indicator,
+                setCurrentSlideIndex == index && {
+                  backgroundColor: COLORS.primaryDark,
+                },
+              ]}
+            />
+          ))}
+
         </View>
 
       </View>
+
     );
+  };
+
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        onMomentumScrollEnd={updateCurrentSlideIndex}
+        pagingEnabled
+        data={slides}
+        contentContainerStyle={{ height: height * 0.75 }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+
+        renderItem={({ item }) => <Slides item={item} />}
+
+      />
+      <Footer />
+    </SafeAreaView>
+  );
+};
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Onboarding" component={Onboarding} options={{ headerShown: false }} />
+      <Stack.Screen name="Regisrty" component={Registry} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
 }
 
+{/*
+export default function Onboarding() {
+  return (
+    <MyStack />
+  );
+}
+*/}
+
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+
+  image: {
+    heigth: '80%',
+    width: width,
+    resizeMode: 'contain',
+  },
+
+  title: {
+    marginTop: 20,
+    textAlign: 'center',
+    color: COLORS.schriftDark,
+  },
+
+  description: {
+    marginTop: 10,
+    maxWidth: '70%',
+    textAlign: 'center',
+    color: COLORS.schriftDark,
+  },
+
+  footerContainer: {
+    height: height * 0.2,
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+
+  indicatorContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+
+  indicator: {
+    height: 10,
+    width: 10,
+    borderRadius: 5,
+    backgroundColor: COLORS.primaryLight,
+    marginHorizontal: 3,
+  },
+
 });
