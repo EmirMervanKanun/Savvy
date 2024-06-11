e997bd8cd1396cc722d06daa9833c0f8ac5f0e5d
mlphung <mai.phung@hof-university.de>
2024-06-11 11:36:06 +0200

    registry fertig yippie

--- Screens/Onboarding/Registry.js
+++ Screens/Onboarding/Registry.js
@@ -1,61 +1,97 @@
 import { StyleSheet, View, ScrollView } from 'react-native';
-import COLORS from '../../Components/Farben';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import Header from '../../Components/TextComponents/Header';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import SelectProfilePicture from '../../Components/Selection/SelectProfilePicture';
 import InputText from '../../Components/Inputfelder/InputText'
 import CurrencySmall from '../../Components/Dropdowns/CurrencySmall';
 import Button from '../../Components/Buttons/Button';
 
-export default function Registry() {
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { CommonActions } from '@react-navigation/native';
+
+import HomeStart from '../Home/Home';
+import Onboarding from '../Onboarding/Onboarding';
+
+const Stack = createNativeStackNavigator();
+
+function RegistryOnboarding ({ navigation }) {
   return (
     <SafeAreaProvider>
-    <ScrollView contentContainerStyle={styles.container} style={styles.contentContainer}>
-      <View style={{paddingBottom: 40}}>
+      <ScrollView contentContainerStyle={styles.container} style={styles.contentContainer}>
+        <View style={{ paddingBottom: 40 }}>
           <SelectProfilePicture />
-      </View>
-
-      <View style={styles.containerInput}>
-
-        <View style={styles.input}>
-          <Header>Vorname</Header>
-          <InputText placeholder="Vorname" />
         </View>
 
-        <View style={styles.input}>
-          <Header>Nachname</Header>
-          <InputText placeholder="Nachname" />
-        </View>
+        <View style={styles.containerInput}>
 
-        <View style={styles.input}>
-          <Header>Startkapital</Header>
-          <InputText placeholder="Gib dein Startkapital ein" />
-          <View style={styles.currency}>
-          <CurrencySmall />
+          <View style={styles.input}>
+            <Header>Vorname</Header>
+            <InputText placeholder="Vorname" />
+          </View>
+
+          <View style={styles.input}>
+            <Header>Nachname</Header>
+            <InputText placeholder="Nachname" />
+          </View>
+
+          <View style={styles.input}>
+            <Header>Startkapital</Header>
+            <InputText placeholder="Gib dein Startkapital ein" />
+            <View style={styles.currency}>
+              <CurrencySmall />
+            </View>
           </View>
         </View>
-      </View>
 
-      <View style={styles.containerButton}>
-        <Button props={{
-          size: 'mid',
-          color: 'blue',
-          img: require('../../Icons/Button/cancel.png'),
-          text: 'Abbrechen',
-        }}
-        />
+        <View style={styles.containerButton}>
+          <Button props={{
+            size: 'mid',
+            color: 'blue',
+            img: require('../../Icons/Button/cancel.png'),
+            text: 'Abbrechen',
+            onPress: () => navigation.dispatch(
+              CommonActions.navigate({
+                name: 'Onboarding',
+              },
+              )
+            )
+          }}
+          />
 
-        <Button props={{
-          size: 'mid',
-          color: 'blue',
-          img: require('../../Icons/Button/save.png'),
-          text: 'Speichern',
-        }}
-        />
-      </View>
-    </ScrollView>
+          <Button props={{
+            size: 'mid',
+            color: 'blue',
+            img: require('../../Icons/Button/save.png'),
+            text: 'Speichern',
+            onPress: () => navigation.dispatch(
+              CommonActions.navigate({
+                name: 'Home',
+              },
+              )
+            )
+          }}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaProvider>
+  );
+}
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Registry" component={RegistryOnboarding} options={{headerShown: false}} />
+      <Stack.Screen name="Home" component={HomeStart} options={{ headerShown: false }} />
+      <Stack.Screen name="Onboarding" component={Onboarding} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+}
+
+export default function Registry() {
+  return (
+    <MyStack />
   );
 }
 
@@ -70,6 +106,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingBottom: 150,
+    backgroundColor: 'white',
   },
   containerInput: {
     gap: 32,
